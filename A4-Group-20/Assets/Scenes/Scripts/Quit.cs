a40f89b5cdc32482eb2be049efb8ef754502803f
youssefhouri <youssef.houri@outlook.com>
2021-11-17 21:44:02 -0500

    Quit fixed

--- A4-Group-20/Assets/Scenes/Scripts/Quit.cs
+++ A4-Group-20/Assets/Scenes/Scripts/Quit.cs
@@ -3,22 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Quit : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void Exit()
+{   public void QuiteGame()
     {
             Application.Quit();
+            Debug.Log("Quit!");
     }
 
 }
